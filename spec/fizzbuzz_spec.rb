37a4fc066c1b7c4ea08a34212cdad0114ea2165b
sara <sara@huhbub.com>
2016-11-04 14:37:24 +0000

    passed all tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,4 +3,13 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+  it 'returns "fizz" if number is divisible by 3' do
+    expect(fizzbuzz(4)).to eq 4
+  end
+  it 'returns "buzz" if number is divisible by 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+  it 'returns "fizzbuzz" if number is divisible by 3 and 5' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
 end
